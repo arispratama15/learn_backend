57764f69c7219fd26258908cd88389d3e4c0ed1e
Aris Pratama <arispratama15@gmail.com>
2021-03-06 16:14:38 +0700

    add math in backend

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const { request } = require('express');
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/', function(req, res){
+  var harga = Number(req.query.id);
+  var pajak = harga * 10 / 100;
+  var totalHarga = harga + pajak;
+  res.send('Harga Vibe + pajak: ' + totalHarga);
+});
+
+app.listen(3000);
+console.log('Express started on port 3000');
